14892ccdd7b2df77e836bb1bae76799872ce4965
Daniel <danielbuendiasmr@gmail.com>
2019-04-11 22:43:27 +0200

    Adaptación de las migraciones para la base de datos

--- database/migrations/2019_04_09_201631_create_comments_table.php
+++ database/migrations/2019_04_09_201631_create_comments_table.php
@@ -15,7 +15,7 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('text');
+            $table->longText('text');
             $table->unsignedInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->unsignedInteger('apartment_id');
