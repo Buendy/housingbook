205c8ef874c3d195d2a24904a7378943240db5f0
Buendy <danielbuendiasmr@gmail.com>
2019-04-25 19:37:36 +0000

    Añadidas validaciones a registro de usuarios

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,12 +49,12 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255', 'regex:/^[a-zA-Z ]*$/'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'lastname' => ['required','string'],
+            'lastname' => ['required','string', 'regex:/^[a-zA-Z ]*$/'],
             'address' => ['required','string'],
-            'phone' => ['required','string'],
+            'phone' => ['required','string', 'regex:/^[9|6|7|8][0-9]{8}$/'],
             'conditions' => ['required']
         ]);
     }
