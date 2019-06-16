77969ab7b369308401c1578006aa2325b7bcd74e
root <robbertto.gomez@gmail.com>
2019-06-16 16:30:04 +0000

    Fotos

--- database/factories/PhotoFactory.php
+++ database/factories/PhotoFactory.php
@@ -1,11 +1,10 @@
 <?php
 
 use Faker\Generator as Faker;
-use App\Photo;
 
 $factory->define(App\Photo::class, function (Faker $faker) {
     return [
-        'local_url' => Photo::image(storage_path() . '/app/public/photos', 600, 350, 'city', false),
+        'local_url' => \Faker\Provider\Image::image(storage_path() . '/app/public/photos', 600, 350, 'city', false),
         'apartment_id' => \App\Apartment::all()->random()->id,
     ];
 });
